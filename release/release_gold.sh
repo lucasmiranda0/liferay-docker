398d471c23a362494a1d9ec5bcb237cc4ad746c1
Lucas Miranda <lucas.miranda@liferay.com>
2024-12-17 14:07:30 -0300

    check the value of LIFERAY_RELEASE_PREPARE_NEXT_RELEASE_BRANCH

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -74,45 +74,51 @@ function main {
 	fi
 
 	check_usage
-
-	check_supported_versions
-
-	init_gcs
-
-	lc_time_run promote_packages
-
-	lc_time_run tag_release
-
-	promote_boms xanadu
-
-	if [[ ! $(echo "${_PRODUCT_VERSION}" | grep "q") ]] &&
-	   [[ ! $(echo "${_PRODUCT_VERSION}" | grep "7.4") ]]
-	then
-		lc_log INFO "Do not update product_info.json for quarterly and 7.4 releases."
-
-		lc_time_run generate_product_info_json
-
-		lc_time_run upload_product_info_json
-	fi
-
-	lc_time_run generate_releases_json
-
-	lc_time_run test_boms
-
-	lc_time_run add_patcher_project_version
-
-	lc_background_run clone_repository liferay-portal-ee
-
-	lc_wait
-
-	lc_time_run clean_portal_repository
+	echo "${LIFERAY_RELEASE_PREPARE_NEXT_RELEASE_BRANCH}"
 
 	if [ $(echo "${LIFERAY_RELEASE_PREPARE_NEXT_RELEASE_BRANCH}" | grep -i "yes") ]
 	then
-		lc_time_run prepare_next_release_branch
-
-		lc_time_run update_release_info_date
+		echo "YES YES YES"
 	fi
+
+	# check_supported_versions
+
+	# init_gcs
+
+	# lc_time_run promote_packages
+
+	# lc_time_run tag_release
+
+	# promote_boms xanadu
+
+	# if [[ ! $(echo "${_PRODUCT_VERSION}" | grep "q") ]] &&
+	#    [[ ! $(echo "${_PRODUCT_VERSION}" | grep "7.4") ]]
+	# then
+	# 	lc_log INFO "Do not update product_info.json for quarterly and 7.4 releases."
+
+	# 	lc_time_run generate_product_info_json
+
+	# 	lc_time_run upload_product_info_json
+	# fi
+
+	# lc_time_run generate_releases_json
+
+	# lc_time_run test_boms
+
+	# lc_time_run add_patcher_project_version
+
+	# lc_background_run clone_repository liferay-portal-ee
+
+	# lc_wait
+
+	# lc_time_run clean_portal_repository
+
+	# if [ $(echo "${LIFERAY_RELEASE_PREPARE_NEXT_RELEASE_BRANCH}" | grep -i "yes") ]
+	# then
+	# 	lc_time_run prepare_next_release_branch
+
+	# 	lc_time_run update_release_info_date
+	# fi
 
 	#lc_time_run upload_to_docker_hub
 }
