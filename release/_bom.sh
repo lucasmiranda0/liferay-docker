a1d3da7745cfcc34c4e607c0494761cd06f28ff5
Davi Santos <davi.santos4357@gmail.com>
2025-07-14 13:31:16 -0300

    LPD-60457 Standardize awk options to long format

--- release/_bom.sh
+++ release/_bom.sh
@@ -272,7 +272,7 @@ function generate_pom_release_bom {
 	echo "" >> "${pom_file_name}"
 
 	find "${_PROJECTS_DIR}/liferay-portal-ee/modules/.releng" -name '*.properties' -print0 | \
-		xargs -0 awk -F= '/^artifact.url=/  { print $2 }' \
+		xargs -0 awk --field-separator '=' '/^artifact.url=/ { print $2 }' \
 		> /tmp/artifact_urls.txt
 
 	for artifact_file in $(
@@ -478,7 +478,7 @@ function copy_tld {
 			grep \
 				--extended-regexp "(/build/|/classes/|/gradleTest/|/sdk/|/test/|/testIntegration/)" \
 				--invert-match | \
-			awk -F "/" '{print $NF, $0}' | \
+			awk --field-separator '/' '{print $NF, $0}' | \
 			sort -k 1,1 -u | \
 			awk '{print $2}')
 	do
