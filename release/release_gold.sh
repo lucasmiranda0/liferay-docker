4b4801ac30c893d81d696b199efed1dcfcbac972
Lucas Miranda <lucas.miranda@liferay.com>
2025-09-10 16:21:56 -0300

    LPD-63650 Remove not needed code

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -127,8 +127,6 @@ function main {
 
 	lc_time_run update_release_info_date
 
-	#lc_time_run upload_to_docker_hub
-
 	lc_time_run add_patcher_project_version
 
 	lc_time_run upload_to_docker_hub "release-gold"
